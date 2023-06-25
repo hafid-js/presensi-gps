dc7c03a47240e99538e72e140c0e6dd5d39c4284
hafidjs <afidzpratama@gmail.com>
2023-06-25 12:59:11 +0700

    create login & dashboard page

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+
+Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index']);
