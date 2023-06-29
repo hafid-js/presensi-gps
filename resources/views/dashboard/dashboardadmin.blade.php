c364b9e00c2dcdbecbcb88fce4c0f488dc8732ee
hafidjs <afidzpratama@gmail.com>
2023-06-29 19:28:55 +0700

    create widget dashboard

--- resources/views/dashboard/dashboardadmin.blade.php
+++ resources/views/dashboard/dashboardadmin.blade.php
@@ -23,23 +23,27 @@
                         <div class="card-body">
                             <div class="row align-items-center">
                                 <div class="col-auto">
-                                    <span class="bg-primary text-white avatar">
+                                    <span class="bg-success text-white avatar">
                                         <!-- Download SVG icon from http://tabler-icons.io/i/currency-dollar -->
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <svg xmlns="http://www.w3.org/2000/svg"
+                                            class="icon icon-tabler icon-tabler-fingerprint" width="24" height="24"
+                                            viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                            stroke-linecap="round" stroke-linejoin="round">
                                             <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                            <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
-                                            <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2"></path>
-                                            <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
-                                            <path d="M21 21v-2a4 4 0 0 0 -3 -3.85"></path>
-                                         </svg>
+                                            <path d="M18.9 7a8 8 0 0 1 1.1 5v1a6 6 0 0 0 .8 3"></path>
+                                            <path d="M8 11a4 4 0 0 1 8 0v1a10 10 0 0 0 2 6"></path>
+                                            <path d="M12 11v2a14 14 0 0 0 2.5 8"></path>
+                                            <path d="M8 15a18 18 0 0 0 1.8 6"></path>
+                                            <path d="M4.9 19a22 22 0 0 1 -.9 -7v-1a8 8 0 0 1 12 -6.95"></path>
+                                        </svg>
                                     </span>
                                 </div>
                                 <div class="col">
                                     <div class="font-weight-medium">
-                                        132
+                                        {{ $rekappresensi->jmlhadir }}
                                     </div>
                                     <div class="text-muted">
-                                        Karyawan
+                                        Karyawan Hadir
                                     </div>
                                 </div>
                             </div>
@@ -68,7 +72,7 @@
                                 </div>
                                 <div class="col">
                                     <div class="font-weight-medium">
-                                        132
+                                        {{ $rekapizin->jmlizin != null ? $rekapizin->jmlizin : 0 }}
                                     </div>
                                     <div class="text-muted">
                                         Karyawan Izin
@@ -96,7 +100,7 @@
                                 </div>
                                 <div class="col">
                                     <div class="font-weight-medium">
-                                        132
+                                        {{ $rekapizin->jmlsakit != null ? $rekapizin->jmlsakit : 0 }}
                                     </div>
                                     <div class="text-muted">
                                         Karyawan Sakit
@@ -123,7 +127,7 @@
                                 </div>
                                 <div class="col">
                                     <div class="font-weight-medium">
-                                        132
+                                        {{ $rekappresensi->jmlterlambat }}
                                     </div>
                                     <div class="text-muted">
                                         Karyawan Terlambat
